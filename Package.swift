088dd6b44813ff843c4c16dc360a50dadf02aaaa
Cole Moore <Cartisim@icloud.com>
2025-06-29 22:07:39 +0800

    Added README.md

--- Package.swift
+++ Package.swift
@@ -18,8 +18,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "3.12.3")),
         .package(url: "https://github.com/needletails/double-ratchet-kit.git", .upToNextMajor(from: "1.0.0")),
-//        .package(path: "../double-ratchet-kit"),
-        .package(url: "git@github.com:needletails/needletail-crypto.git", .upToNextMajor(from: "1.0.12")),
+        .package(url: "https://github.com/needletails/needletail-crypto.git", .upToNextMajor(from: "1.0.12")),
         .package(url: "https://github.com/needletails/needletail-logger.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/needletails/needletail-algorithms.git", .upToNextMajor(from: "2.0.0")),
     ],
